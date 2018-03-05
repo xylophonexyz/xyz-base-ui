5f4b2c5aee41fca93959bbee5dddbb6b67307999
Xylophone <staff@xylophonexyz.com>
2018-03-05 14:22:01 -0800

    auth interceptor properly retries after reauth

--- src/app/providers/auth-interceptor.service.ts
+++ src/app/providers/auth-interceptor.service.ts
@@ -3,6 +3,8 @@ import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/finally';
 import {Observable} from 'rxjs/Observable';
 import {AuthService} from './auth.service';
 import {NavbarDelegateService} from './navbar-delegate.service';
@@ -19,24 +21,19 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request).do(null, error => {
+    return next.handle(request).catch(error => {
       if (error instanceof HttpErrorResponse) {
         if (error.status === 401 && !AuthInterceptor.isTokenRequest(error.url)) {
           // inform the ui that a re-auth is about to take place
           this.addInfoBannerToNav();
-          // perform re-authentication
-          return Observable.fromPromise(new Promise((resolve, reject) => {
-            const auth: AuthService = this.injector.get(AuthService);
-            auth.authenticate(null, true).then(() => {
-              if (request.headers) {
-                request.headers.set('Authorization', `Bearer ${auth.accessToken}`);
-              }
-              next.handle(request).do(res => {
-                this.removeInfoBannerFromNav();
-                resolve(res);
-              }, reject);
-            }).catch(reject);
-          }));
+          // perform re-auth and retry the request with the new token
+          const auth: AuthService = this.injector.get(AuthService);
+          return Observable.fromPromise(auth.authenticate(null, true)).switchMap(() => {
+            const requestWithNewToken = request.clone({
+              headers: request.headers.set('Authorization', `Bearer ${auth.accessToken}`)
+            });
+            return next.handle(requestWithNewToken);
+          }).finally(() => this.removeInfoBannerFromNav());
         } else {
           return Observable.throw(error);
         }
